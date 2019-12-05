851d07dd6b4724ad6da9a6cfea37aaa5881b2b65
Stanley Huang <huang.stanley050@gmail.com>
2019-12-05 17:34:36 +1100

    added condition for routes: /api/graphql, /playground, /

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ import path from "path";
 const port = process.env.PORT || 8000;
 const opts = {
   port: port,
-  endpoint: "/api/graphql"
+  endpoint: "/api/graphql",
+  playground: "/playground"
 };
-server.express.get("/", (req, res) => {
+server.express.get("*", (req, res, next) => {
+  if (req.url === opts.playground || req.url === opts.endpoint) {
+    // Return next() so that the GraphQLServer will handle it
+    return next();
+  }
   res.sendFile(path.join(__dirname, "./client/index.html"));
 });
 
