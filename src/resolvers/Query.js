f312ecafc96f68e13f3564778eb5244db8104e9a
Stanley Huang <huang.stanley050@gmail.com>
2019-12-07 16:22:05 +1100

    able to call opendb api and get result returned in graphql

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,22 +1,42 @@
+import axios from "axios";
+const apiRoute = `https://opentdb.com/api.php?`;
+const category = {
+  general: 9,
+  books: 10,
+  film: 11,
+  music: 12,
+  musical: 13,
+  tv: 14,
+  videoGame: 15,
+  boardGame: 16,
+  scienceNature: 17,
+  scienceComputer: 18,
+  scienceMath: 19,
+  mythology: 20,
+  sports: 21,
+  geo: 22,
+  history: 23,
+  politics: 24,
+  art: 25,
+  celebrities: 26,
+  animals: 27,
+  vehicle: 28,
+  comics: 29,
+  scienceGadget: 30,
+  animeManga: 31,
+  cartoon: 32
+};
 const Query = {
   hello: () => "Hello world",
-  trivias: (parents, args, ctx, info) => {
-    return [
-      {
-        category: args.data.category,
-        difficulty: args.data.difficulty,
-        question: "What is your name?",
-        correctAnswer: "Jin Kazama",
-        incorrectAnswers: [{ wrong: "1" }, { wrong: "2" }, { wrong: "2" }]
-      },
-      {
-        category: args.data.category,
-        difficulty: args.data.difficulty,
-        question: "What is your age?",
-        correctAnswer: "Jin Kazama",
-        incorrectAnswers: [{ wrong: "1" }, { wrong: "2" }, { wrong: "2" }]
-      }
-    ];
+  trivias: async (parents, args, ctx, info) => {
+    const result = await axios.get(
+      `${apiRoute}amount=${args.data.numberQuestions}&category=${
+        category[args.data.category]
+      }&difficulty=${args.data.difficulty}`
+    );
+
+    console.log(result.data);
+    return result.data.results;
   }
 };
 
