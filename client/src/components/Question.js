f4ef46d82c786eb8b4c05d3af6764561fff046f1
Stanley Huang <huang.stanley050@gmail.com>
2019-12-17 14:38:31 +1100

    issue sorted, get the answers array from parent Questions

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,14 +1,6 @@
 import React, { useState } from "react";
 import { Radio, Form } from "semantic-ui-react";
 
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
 const decodeHTML = html => {
   var txt = document.createElement("textarea");
   txt.innerHTML = html;
@@ -16,25 +8,18 @@ const decodeHTML = html => {
 };
 
 const Question = props => {
-  let answers = [...props.data.incorrect_answers, props.data.correct_answer];
   const [selectedAnswer, setAnswer] = useState("");
   const handleChange = (e, { value }) => {
     console.log(value);
     setAnswer(value);
   };
-  answers = shuffle(answers);
-  // let correctAnswer = props.data.correct_answer;
-  // if (props.data) {
-  //   answers = props.data.incorrect_answers;
-  //   answers.push(props.data.correct_answer);
-  //   answers = shuffle(answers);
-  // }
+
   return (
     <Form>
       <Form.Field style={{ margin: "1rem 0" }}>
-        Question: {props.number + 1} <h2>{decodeHTML(props.data.question)}</h2>
+        Question: {props.number + 1} <h2>{decodeHTML(props.question)}</h2>
       </Form.Field>
-      {answers.map((answer, index) => {
+      {props.data.map((answer, index) => {
         return (
           <Form.Field key={index}>
             <Radio
--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,18 +1,35 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import Question from "./Question";
 import { store } from "../store/store";
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
 
 const Questions = props => {
   const globalState = useContext(store);
   const { trivias } = globalState.state;
-  console.log(trivias);
+  //console.log(trivias);
   return (
     <div style={{ width: "80%", margin: "0 auto" }}>
       <h1 style={{ textAlign: "center", marginTop: "2rem" }}>Questions:</h1>
       {trivias.length !== 0
-        ? trivias.map((trivia, index) => (
-            <Question key={trivia.question} data={trivia} number={index} />
-          ))
+        ? trivias.map((trivia, index) => {
+            let answers = [...trivia.incorrect_answers, trivia.correct_answer];
+            answers = shuffle(answers);
+            return (
+              <Question
+                key={trivia.question}
+                question={trivia.question}
+                data={answers}
+                number={index}
+              />
+            );
+          })
         : null}
     </div>
   );
